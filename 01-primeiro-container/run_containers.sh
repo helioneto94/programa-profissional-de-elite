63fe7fda4618a5215ea4e570a210b6c30c77ef77
Helio Neto <helioneto94@gmail.com>
2025-04-02 21:40:39 +0000

    adicionando versões das imagens

--- 01-primeiro-container/run_containers.sh
+++ 01-primeiro-container/run_containers.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
  
-sudo docker pull nginx
-sudo docker container run -d --name meu-servidor nginx
+sudo docker pull nginx:1.27.4-bookworm
+sudo docker container run -d --name meu-servidor nginx:1.27.4-bookworm
 sudo docker container ls
 sudo docker rm -f meu-servidor
 sudo docker container ls -a
--- 02-container-logs/nginx_volume_container.sh
+++ 02-container-logs/nginx_volume_container.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
  
 sudo docker volume create nginx_logs
-sudo docker container run -d -p 8080:80 --name meu-container -v nginx_logs:/var/log/nginx nginx
+sudo docker container run -d -p 8080:80 --name meu-container -v nginx_logs:/var/log/nginx nginx:1.27.4-bookworm
 echo "Pausa de 5 segundos para finalização do container"
 sleep 5
 curl http://localhost:8080
 sudo docker rm -f meu-container
-sudo docker container run -d -p 8080:80 --name meu-container -v nginx_logs:/var/log/nginx nginx
+sudo docker container run -d -p 8080:80 --name meu-container -v nginx_logs:/var/log/nginx nginx:1.27.4-bookworm
 sudo docker exec -it meu-container cat /var/log/nginx/access.log
